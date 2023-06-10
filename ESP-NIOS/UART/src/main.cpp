b517203d952bd4bf1e2e860eff1afbfbc5e9888f
luigirinaldi <luigirinaldi@users.noreply.github.com>
2023-06-10 17:18:14 +0100

    feat: add reset functionality

--- ESP-NIOS/UART/src/main.cpp
+++ ESP-NIOS/UART/src/main.cpp
@@ -1,21 +1,30 @@
 #include <Arduino.h>
 
+#define NIOS_RESET_PIN 18
+#define NIOS_UART_RX   17
+#define NIOS_UART_TX   16
+
 HardwareSerial NIOS(2);
+
+void reset_NIOS();
 
 void setup() {
 
-  NIOS.begin(115200, SERIAL_8N1, 16, 17); 
 
 
   Serial.begin(115200);
-  Serial.println("Hello brothers!");
+  Serial.println("ESP-32\nImperial College London EE2 Project Magenta");
 
+  pinMode(NIOS_RESET_PIN, OUTPUT);
+  reset_NIOS();
+
+  NIOS.begin(115200, SERIAL_8N1, 16, 17); 
   while (!NIOS.available()) {
     delay(500);
-    Serial.println("Waiting for message from nios");
+    Serial.println("Waiting for message from NIOS");
   }
   while (NIOS.available()){
-    Serial.println("Recevied from nios!");
+    Serial.println("Recevied from NIOS:");
     Serial.println(NIOS.readString());
   }
 }
@@ -26,4 +35,14 @@ void loop() {
   NIOS.print(i++);
   NIOS.println("Luigi");
   delay(5000);
+}
+
+void reset_NIOS() {
+  Serial.println("Resetting NIOS");
+  digitalWrite(NIOS_RESET_PIN, LOW);
+  delay(20);
+  digitalWrite(NIOS_RESET_PIN, HIGH);
+  delay(20);
+  digitalWrite(NIOS_RESET_PIN, LOW);
+
 }
