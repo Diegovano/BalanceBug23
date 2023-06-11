683ede0a58feba2f1eb8146dc2cb9949b43e899c
luigirinaldi <luigirinaldi@users.noreply.github.com>
2023-06-11 17:13:14 +0100

    feat: working control of settings from esp to nios

--- ESP-NIOS/UART/src/main.cpp
+++ ESP-NIOS/UART/src/main.cpp
@@ -15,6 +15,8 @@ void setup() {
   Serial.begin(115200);
   Serial.println("ESP-32\nImperial College London EE2 Project Magenta");
 
+
+  // NIOS SETUP
   pinMode(NIOS_RESET_PIN, OUTPUT);
   reset_NIOS();
 
@@ -27,14 +29,37 @@ void setup() {
     Serial.println("Recevied from NIOS:");
     Serial.println(NIOS.readString());
   }
+
+  delay(500);
+  NIOS.printf("E%08x\n", 0x2d0);
+  delay(50);
+  NIOS.printf("G%08x\n", 0x0);
+  delay(50);
+  NIOS.printf("H%08x\n", 0x0000FF);
+  delay(50);
+  NIOS.printf("T%08x\n", 0x2ddd);
+  delay(50);
+  NIOS.printf("A%08x\n", 0x5);
+
+
 }
 
 int i = 0;
 
 void loop() {
-  NIOS.print(i++);
-  NIOS.println("Luigi");
-  delay(5000);
+
+  
+
+  // if(NIOS.available()){
+  //   char msgType = NIOS.read();
+  //   int value = NIOS.readStringUntil('\n').toInt(); 
+  //   Serial.printf("NIOS. type: %c, val: %8x\n", msgType, value);
+  // }
+
+  // delay(20);
+  // NIOS.printf("E%08x\n", i += 2);
+  // Serial.printf("Sent message:E%08x\n", i);
+  // delay(500);
 }
 
 void reset_NIOS() {
