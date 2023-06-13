c1777cea9585dbc40a1af06a865d1cdef9385365
Ezra Reich <64613180+EZePiZy@users.noreply.github.com>
2023-06-13 15:22:23 +0100

    feat: Finish intergration ESP-SERVER for beacon data and triangulation request

--- ESP-NIOS/SERVER-NIOS/SERVER-ESP/src/main.cpp
+++ ESP-NIOS/SERVER-NIOS/SERVER-ESP/src/main.cpp
@@ -8,16 +8,14 @@
 #define SERVER_IP "54.82.44.87"
 #define HTTP_PORT "3001"
 
-const char* serverUrl = "http://54.82.44.87:3001/beacon";
-const char* triangulationUrl = "http://54.82.44.87:3001/api/flag";
+const char *serverUrl = "http://54.82.44.87:3001/beacon";
+const char *triangulationUrl = "http://54.82.44.87:3001/api/flag";
 
 const size_t bufferSize = JSON_OBJECT_SIZE(6);
 const unsigned long pollingInterval = 100;
 
-
-
-
-void setup() {
+void setup()
+{
   // Wifi connection:
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -28,7 +26,8 @@ void setup() {
 // Keeping state variable
 bool isConnected = false;
 
-void loop() {
+void loop()
+{
   // Parse values
   StaticJsonDocument<bufferSize> jsonDoc;
   jsonDoc["distance1"] = 3.5;
@@ -39,12 +38,14 @@ void loop() {
   jsonDoc["rotation3"] = 7;
 
   // Connection to Wifi
-  if (WiFi.status() == WL_CONNECTED && !isConnected) {
+  if (WiFi.status() == WL_CONNECTED && !isConnected)
+  {
     Serial.println("Connected");
     digitalWrite(LED_BUILTIN, HIGH);
     isConnected = true;
   }
-  if (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.println(".");
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
     delay(1000);
@@ -53,8 +54,10 @@ void loop() {
 
   // Build query parameters
   String queryParams;
-  for (const auto& kvp : jsonDoc.as<JsonObject>()) {
-    if (!queryParams.isEmpty()) {
+  for (const auto &kvp : jsonDoc.as<JsonObject>())
+  {
+    if (!queryParams.isEmpty())
+    {
       queryParams += "&";
     }
     queryParams += kvp.key().c_str();
@@ -64,7 +67,8 @@ void loop() {
 
   // Append query parameters to server URL
   String requestUrl = serverUrl;
-  if (!queryParams.isEmpty()) {
+  if (!queryParams.isEmpty())
+  {
     requestUrl += "?";
     requestUrl += queryParams;
   }
@@ -72,16 +76,19 @@ void loop() {
   WiFiClient client;
   HTTPClient http;
 
-  // Sending beacon values to server 
+  // Sending beacon values to server
   http.begin(requestUrl);
   http.addHeader("Content-Type", "application/json");
-  int httpResponseCode = http.GET();
+  int httpResponseCodeBeacon = http.GET();
 
-  if (httpResponseCode > 0) {
+  if (httpResponseCodeBeacon > 0)
+  {
     String response = http.getString();
-    Serial.println("HTTP Response code: " + String(httpResponseCode));
+    Serial.println("HTTP Response code: " + String(httpResponseCodeBeacon));
     Serial.println("Response: " + response);
-  } else {
+  }
+  else
+  {
     Serial.println("Error sending GET request");
   }
   http.end();
@@ -89,17 +96,32 @@ void loop() {
   delay(500);
 
   http.begin(triangulationUrl);
-  int httpResponseCode = http.GET();
-  if (httpResponseCode == HTTP_CODE_OK){
+  int httpResponseCodeTriangulation = http.GET();
+
+  if (httpResponseCodeTriangulation == HTTP_CODE_OK)
+  {
     String response = http.getString();
-    int flagValue = response.toInt();
-    Serial.print("Flag value: ");
-    Serial.println(flagValue);
-  } else {
+    // Serial.println(response);
+    DynamicJsonDocument doc(1024);
+    DeserializationError error = deserializeJson(doc, response);
+
+    if (error)
+    {
+      Serial.print("Error parsing JSON: ");
+      Serial.println(error.c_str());
+    }
+    else
+    {
+      int flagValue = doc["flag"];
+      Serial.print("Flag value: ");
+      Serial.println(flagValue);
+    }
+  }
+  else
+  {
     Serial.print("Error: ");
-    Serial.println(httpResponseCode);
+    Serial.println(httpResponseCodeTriangulation);
   }
   http.end();
   delay(pollingInterval);
-
 }
